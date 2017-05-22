794c884b2254f3ed8a29251e17a86c6a5f73ee00
bfindlay <bfin5330@uni.sydney.edu.au>
2017-05-22 21:06:18 +1000

    adding endpoints

--- API.js
+++ API.js
@@ -3,43 +3,82 @@ const Express = require('express');
 const Router = Express.Router();
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken'); 
+const bcrypt = require('bcrypt-nodejs');
+
 Router.use(bodyParser.json()); // support json encoded bodies
 Router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-//API Verification MiddleWare
-Router.use(function(req, res, next) {
-    console.log('auth middleware');
-    const { body, method, url } = req;
-    if(method === 'POST'){
-        let token = body.token;
-        if(url === '/login' || url === '/user')
-            return next();
-        if (token !== undefined) {
-            jwt.verify(token, 'x1ODH27zt4sMUMW882iwCp3T6cvWBf38', function(err, decoded) {      
-                if (err) {
-                    return res.json({ success: false, message: 'Failed to authenticate token.' });    
-                } else {
-                    req.decoded = decoded;
-                    return next();
-                }
-            });
-        } else {
-            return res.status(403).send({ 
-                success: false, 
-                message: 'No token provided.' 
-            });
-        }
-    }else{
-        // No token needed
-        return next();
-    }
-}); 
+
+Router.post('/login', (req, res) => {
+    const { auth } = req.body;
+    console.log('login request', email, password);
+
+    attemptLogin(auth).then( result => {
+        //successfull login, send auth token to user
+
+    }).catch(err => {
+        //login failed
+    })
+
+    res.sendStatus(200);
+});
+
+Router.post('/', (req, res) => {
+    console.log('success post', req.body);
+    res.sendStatus(200);
+});
+
 
 Router.get('/', (req, res) => {
     console.log('home request');
-})
+    res.send("hello");
+});
 
-Router.post('/login', (req, res) => {
-    console.log('login request');
-})
+Router.post('/sign-up', function(req, res) {
+    const { email, pass } = req.body.auth;
+	hashPassword(pass).then(function(response) {
+        registerUser( name.toLowerCase(), response, email.toLowerCase())
+			.then(function(result) {
+                // Save user to db
+                return res.status(200).send('Success');
+			}).catch(function(err) {
+                return res.status(400).send(err);
+			});
+	});
+});
+
+let registerUser = ( name, password, email) => {
+	//TODO check if exists, resolve or reject depending on result
+	return new Promise(function(resolve, reject) {
+	
+	});
+};
+
+let hashPassword = (password_plain_text) => {
+	return new Promise(function(resolve, reject) {
+		bcrypt.hash(password_plain_text, null,null, function(err, hash) {
+			if (err) reject("Error hashing the password");
+			else {
+				resolve(hash);
+			}
+		});
+	});
+};
+
+const attemptLogin = auth => {
+    return new Promise( (resolve, reject ) => {
+        //check database for a user
+        if(true){
+            resolve({});
+        }else{
+            reject('Wrong details');
+        }
+    })
+}
+
+
+
+
+
+
 module.exports = Router;
