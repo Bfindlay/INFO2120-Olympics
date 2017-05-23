225133c284c6778682ec0a231dd0498b4991094e
louisli0 <louisli486@gmail.com>
2017-05-23 17:34:51 +1000

    query updated

--- statements.sql
+++ statements.sql
@@ -29,7 +29,27 @@ FROM olympics.booking B JOIN olympics.journey J ON (J.journey_id = B.journey_id)
 WHERE booked_for = login.member_id //'login member_id'
 ORDER by J.depart_time ASC;
 
+--Search Journeys
+SELECT depart_time AS Departing, from_place AS From, to_place AS To, nbooked AS Booked,
+FROM Journey
+WHERE depart_time >= date.entered 
+    AND depart_time <= date.entered 
+    AND to_place = location.entered
+ORDER BY depart_time ASC;
 
---
+--Make booking
 
+--Trigger?
+
+
+--Browse Events
+SELECT *
+From Event;
+
+--Browse Event results (Dunno if it works)
+SELECT M.given_name || m.famiy_name AS Medallist, medal AS Medal, E.event_name AS Event
+FROM participates P JOIN Athelete A ON (P.athelete_id = A.member_id) 
+                    JOIN event E ON (P.event_id = E.event_id) 
+                    JOIN member M ON (A.athelete_id = M.member_id)
+ORDER BY Medallist ASC;
 
