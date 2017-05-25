c8bdca93fdde8c91a22ed33e3db14c47a9018d0f
louisli0 <louisli486@gmail.com>
2017-05-25 11:24:57 +1000

    updated queries, added more information into pages

--- src/components/Details.js
+++ src/components/Details.js
@@ -10,17 +10,15 @@ class Details extends Component {
 
     render(){
         const{ country_code, family_name, given_names, member_id, title, accommodation } = this.props.DB;
-      
         return(
             <div>
                  <div className="details-container">
                     <h1> Details </h1>
                     <div className="details-module">
                         <h3>Member ID: {member_id}</h3>
-                        <h3>Name: {family_name}</h3>
-                        <h3>Type: {member_id}</h3>
-                        <h3>Accomodation: {accommodation}</h3>
-
+                        <h3>Name: {title} {given_names} {family_name}</h3>
+                        <h3>Type: {member_id} for {country_code}</h3>
+                        <h3>Accommodation: {accommodation}</h3>
                     </div>
                 </div>
             </div>
